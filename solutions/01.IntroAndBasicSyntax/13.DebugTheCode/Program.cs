af7555b9ad48c0cfd7482770c2237d10dc9b439e
Teodor Kurtev <teodor.ivanov92@gmail.com>
2022-01-09 18:25:20 +0200

    feat: added project lab templates

---
+++ solutions/01.IntroAndBasicSyntax/13.DebugTheCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _13.DebugTheCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
